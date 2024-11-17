59dcfb01173fad5f32908ab4960bc8595cd745ac
eggfriedrice <eggfriedricew.g.o@gmail.com>
2024-11-17 09:55:44 +0000

    add projects page, move member from main group to bare

---
+++ src/app/(main)/workspaces/[workspaceId]/projects/page.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+
+import { redirect } from "next/navigation"
+
+import { getServerSession } from "@/features/auth/lib/queries"
+
+export default async function Projects() {
+  const session = await getServerSession()
+
+  if (!session) redirect("/sign-in")
+
+  return (
+    <div className="flex-1">
+      <h1 className="text-xl font-bold">Projects</h1>
+    </div>
+  )
+}
