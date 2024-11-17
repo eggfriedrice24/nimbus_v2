465167089677329c59a1d0efb9861a321a8c7b5b
eggfriedrice <eggfriedricew.g.o@gmail.com>
2024-11-17 10:38:21 +0000

    render dynamic list of members

--- src/features/members/components/member-list.tsx
+++ src/features/members/components/member-list.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { DotsHorizontalIcon } from "@radix-ui/react-icons"
 import { Trash2 } from "lucide-react"
 
@@ -23,8 +25,15 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
+import { DottedSeparator } from "@/components/ui/separator"
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id"
+
+import { useGetMembers } from "../services/use-get-members"
 
 export function MemberList() {
+  const workspaceId = useWorkspaceId()
+  const { data } = useGetMembers({ workspaceId })
+
   return (
     <Card className="border-none bg-secondary/50">
       <CardHeader>
@@ -34,133 +43,82 @@ export function MemberList() {
         </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-6">
-        <div className="flex items-center justify-between space-x-4">
-          <div className="flex items-center space-x-4">
-            <Avatar>
-              <AvatarImage src="/avatars/01.png" />
-              <AvatarFallback>OM</AvatarFallback>
-            </Avatar>
-            <div>
-              <p className="text-sm font-medium leading-none">Sofia Davis</p>
-              <p className="text-sm text-muted-foreground">m@example.com</p>
-            </div>
-          </div>
-          <div className="flex items-center space-x-2">
-            <Popover>
-              <PopoverTrigger asChild>
-                <Button className="ml-auto" size="icon">
-                  <DotsHorizontalIcon className="size-4" />
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="p-0" align="end">
-                <Command>
-                  <CommandInput placeholder="Select new role..." />
-                  <CommandList>
-                    <CommandEmpty>No roles found.</CommandEmpty>
-                    <CommandGroup>
-                      <CommandItem className="flex flex-col items-start px-4 py-2">
-                        <p>Viewer</p>
-                        <p className="text-sm text-muted-foreground">
-                          Can view and comment.
-                        </p>
-                      </CommandItem>
-                      <CommandItem className="flex flex-col items-start px-4 py-2">
-                        <p>Developer</p>
-                        <p className="text-sm text-muted-foreground">
-                          Can view, comment and edit.
-                        </p>
-                      </CommandItem>
-                      <CommandItem className="flex flex-col items-start px-4 py-2">
-                        <p>Billing</p>
-                        <p className="text-sm text-muted-foreground">
-                          Can view, comment and manage billing.
-                        </p>
-                      </CommandItem>
-                      <CommandItem className="flex flex-col items-start px-4 py-2">
-                        <p>Owner</p>
-                        <p className="text-sm text-muted-foreground">
-                          Admin-level access to all resources.
-                        </p>
-                      </CommandItem>
-                    </CommandGroup>
-                  </CommandList>
-                </Command>
-              </PopoverContent>
-            </Popover>
-
-            <Button
-              variant="destructive"
-              size="icon"
-              aria-label="Delete member from workspace"
+        {data?.data?.map((m, i) => (
+          <>
+            <div
+              className="flex items-center justify-between space-x-4"
+              key={m.id}
             >
-              <Trash2 className="size-4" />
-            </Button>
-          </div>
-        </div>
-        <div className="flex items-center justify-between space-x-4">
-          <div className="flex items-center space-x-4">
-            <Avatar>
-              <AvatarImage src="/avatars/02.png" />
-              <AvatarFallback>JL</AvatarFallback>
-            </Avatar>
-            <div>
-              <p className="text-sm font-medium leading-none">Jackson Lee</p>
-              <p className="text-sm text-muted-foreground">p@example.com</p>
-            </div>
-          </div>
+              <div className="flex items-center space-x-4">
+                <Avatar>
+                  <AvatarImage src={m.user.image ?? "/avatars/01.png"} />
+                  <AvatarFallback>{m.user.name.charAt(0)}</AvatarFallback>
+                </Avatar>
+                <div>
+                  <p className="text-sm font-medium leading-none">
+                    {m.user.name}
+                  </p>
+                  <p className="text-sm text-muted-foreground">
+                    {m.user.email}
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-center space-x-2">
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button className="ml-auto" size="icon">
+                      <DotsHorizontalIcon className="size-4" />
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="p-0" align="end">
+                    <Command>
+                      <CommandInput placeholder="Select new role..." />
+                      <CommandList>
+                        <CommandEmpty>No roles found.</CommandEmpty>
+                        <CommandGroup>
+                          <CommandItem className="flex flex-col items-start px-4 py-2">
+                            <p>Viewer</p>
+                            <p className="text-sm text-muted-foreground">
+                              Can view and comment.
+                            </p>
+                          </CommandItem>
+                          <CommandItem className="flex flex-col items-start px-4 py-2">
+                            <p>Developer</p>
+                            <p className="text-sm text-muted-foreground">
+                              Can view, comment and edit.
+                            </p>
+                          </CommandItem>
+                          <CommandItem className="flex flex-col items-start px-4 py-2">
+                            <p>Billing</p>
+                            <p className="text-sm text-muted-foreground">
+                              Can view, comment and manage billing.
+                            </p>
+                          </CommandItem>
+                          <CommandItem className="flex flex-col items-start px-4 py-2">
+                            <p>Owner</p>
+                            <p className="text-sm text-muted-foreground">
+                              Admin-level access to all resources.
+                            </p>
+                          </CommandItem>
+                        </CommandGroup>
+                      </CommandList>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
 
-          <div className="flex items-center space-x-2">
-            <Popover>
-              <PopoverTrigger asChild>
-                <Button className="ml-auto" size="icon">
-                  <DotsHorizontalIcon className="size-4" />
+                <Button
+                  variant="destructive"
+                  size="icon"
+                  aria-label="Delete member from workspace"
+                >
+                  <Trash2 className="size-4" />
                 </Button>
-              </PopoverTrigger>
-              <PopoverContent className="p-0" align="end">
-                <Command>
-                  <CommandInput placeholder="Select new role..." />
-                  <CommandList>
-                    <CommandEmpty>No roles found.</CommandEmpty>
-                    <CommandGroup className="p-1.5">
-                      <CommandItem className="flex flex-col items-start px-4 py-2">
-                        <p>Viewer</p>
-                        <p className="text-sm text-muted-foreground">
-                          Can view and comment.
-                        </p>
-                      </CommandItem>
-                      <CommandItem className="flex flex-col items-start px-4 py-2">
-                        <p>Developer</p>
-                        <p className="text-sm text-muted-foreground">
-                          Can view, comment and edit.
-                        </p>
-                      </CommandItem>
-                      <CommandItem className="flex flex-col items-start px-4 py-2">
-                        <p>Billing</p>
-                        <p className="text-sm text-muted-foreground">
-                          Can view, comment and manage billing.
-                        </p>
-                      </CommandItem>
-                      <CommandItem className="flex flex-col items-start px-4 py-2">
-                        <p>Owner</p>
-                        <p className="text-sm text-muted-foreground">
-                          Admin-level access to all resources.
-                        </p>
-                      </CommandItem>
-                    </CommandGroup>
-                  </CommandList>
-                </Command>
-              </PopoverContent>
-            </Popover>
+              </div>
+            </div>
 
-            <Button
-              variant="destructive"
-              size="icon"
-              aria-label="Delete member from workspace"
-            >
-              <Trash2 className="size-4" />
-            </Button>
-          </div>
-        </div>
+            {i !== data.data.length - 1 && <DottedSeparator />}
+          </>
+        ))}
       </CardContent>
     </Card>
   )
