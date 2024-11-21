68c68d6444fbcabc5d3e713ee3ea574eb69006db
eggfriedrice <eggfriedricew.g.o@gmail.com>
2024-11-21 16:24:13 +0000

    add data-table pagination

--- src/hooks/use-data-table.ts
+++ src/hooks/use-data-table.ts
@@ -10,32 +10,99 @@ import {
   getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
-  type ColumnDef,
+  type PaginationState,
+  type TableOptions,
+  type Updater,
   type VisibilityState,
 } from "@tanstack/react-table"
+import { parseAsInteger, useQueryState, type UseQueryStateOptions } from "nuqs"
 
-interface UseDataTableProps<TData, TValue> {
-  data: TData[]
-  columns: ColumnDef<TData, TValue>[]
+interface UseDataTableProps<TData>
+  extends Omit<
+      TableOptions<TData>,
+      | "state"
+      | "pageCount"
+      | "getCoreRowModel"
+      | "manualFiltering"
+      | "manualPagination"
+      | "manualSorting"
+    >,
+    Required<Pick<TableOptions<TData>, "pageCount">> {
+  history?: "push" | "replace"
+  scroll?: boolean
+  shallow?: boolean
+  throttleMs?: number
+  debounceMs?: number
+  clearOnDefault?: boolean
 }
 
-export function useDataTable<TData, TValue>({
-  data,
-  columns,
-}: UseDataTableProps<TData, TValue>) {
+export function useDataTable<TData>({
+  pageCount = -1,
+  history = "replace",
+  scroll = false,
+  shallow = true,
+  throttleMs = 50,
+  debounceMs = 300,
+  clearOnDefault = false,
+  initialState,
+  ...props
+}: UseDataTableProps<TData>) {
   const [rowSelection, setRowSelection] = React.useState({})
   const [columnVisibility, setColumnVisibility] =
     React.useState<VisibilityState>({})
 
+  const queryStateOptions = React.useMemo<
+    Omit<UseQueryStateOptions<string>, "parse">
+  >(() => {
+    return {
+      history,
+      scroll,
+      shallow,
+      throttleMs,
+      debounceMs,
+      clearOnDefault,
+    }
+  }, [history, scroll, shallow, throttleMs, debounceMs, clearOnDefault])
+
+  const [page, setPage] = useQueryState(
+    "page",
+    parseAsInteger.withOptions(queryStateOptions).withDefault(1)
+  )
+  const [perPage, setPerPage] = useQueryState(
+    "perPage",
+    parseAsInteger
+      .withOptions(queryStateOptions)
+      .withDefault(initialState?.pagination?.pageSize ?? 10)
+  )
+
+  const pagination: PaginationState = {
+    pageIndex: page - 1,
+    pageSize: perPage,
+  }
+
+  function onPaginationChange(updaterOrValue: Updater<PaginationState>) {
+    if (typeof updaterOrValue === "function") {
+      const newPagination = updaterOrValue(pagination)
+      void setPage(newPagination.pageIndex + 1)
+      void setPerPage(newPagination.pageSize)
+    } else {
+      void setPage(updaterOrValue.pageIndex + 1)
+      void setPerPage(updaterOrValue.pageSize)
+    }
+  }
+
   const table = useReactTable({
-    data,
-    columns,
+    ...props,
+    initialState,
+    pageCount,
     state: {
+      pagination,
       columnVisibility,
       rowSelection,
     },
     enableRowSelection: true,
     onRowSelectionChange: setRowSelection,
+    onPaginationChange,
     onColumnVisibilityChange: setColumnVisibility,
     getCoreRowModel: getCoreRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
